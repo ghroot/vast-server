d1b7bc0ecf2244a641b46ee5570447f901da5c08
Marcus Lagerström <>
2017-11-18 18:22:07 +0100

    Pickups don't need to scan.

--- src/main/java/com/vast/system/CreationManager.java
+++ src/main/java/com/vast/system/CreationManager.java
@@ -47,7 +47,6 @@ public class CreationManager extends ProfiledBaseSystem {
 				.add(Spatial.class)
 				.add(Collision.class)
 				.add(Pickup.class)
-				.add(Scan.class)
 				.build(world);
 
 		aiArchetype = new ArchetypeBuilder()
