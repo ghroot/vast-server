d62738bdf191b9058bf6787dae3a6ba933afc076
Marcus Lagerström <>
2017-11-29 22:28:52 +0100

    Fix negative player sub type due to hash code for name being negative sometimes.

--- src/main/java/com/vast/system/PeerEntitySystem.java
+++ src/main/java/com/vast/system/PeerEntitySystem.java
@@ -63,7 +63,7 @@ public class PeerEntitySystem extends BaseSystem {
 	}
 
 	private void createPeerEntity(VastPeer peer) {
-		int playerEntity = creationManager.createPlayer(peer.getName(), peer.getName().hashCode() % 3, peer instanceof FakePeer);
+		int playerEntity = creationManager.createPlayer(peer.getName(), Math.abs(peer.getName().hashCode()) % 3, peer instanceof FakePeer);
 		entitiesByPeer.put(peer.getName(), playerEntity);
 		logger.info("Creating peer entity: {} for {} at {}", playerEntity, peer.getName(), transformMapper.get(playerEntity).position);
 	}
