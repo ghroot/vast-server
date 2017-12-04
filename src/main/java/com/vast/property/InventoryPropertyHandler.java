bf51420bcb3cf82c56e90091ebf587f1b1be5c26
Marcus Lagerström <>
2017-12-04 23:53:08 +0100

    Only sync inventory if it has changed.

--- src/main/java/com/vast/property/InventoryPropertyHandler.java
+++ src/main/java/com/vast/property/InventoryPropertyHandler.java
@@ -5,9 +5,13 @@ import com.nhnent.haste.protocol.data.DataObject;
 import com.vast.MessageCodes;
 import com.vast.Properties;
 import com.vast.component.Inventory;
+import com.vast.component.SyncHistory;
+
+import java.util.Arrays;
 
 public class InventoryPropertyHandler implements PropertyHandler {
 	private ComponentMapper<Inventory> inventoryMapper;
+	private ComponentMapper<SyncHistory> syncHistoryMapper;
 
 	@Override
 	public int getProperty() {
@@ -17,8 +21,21 @@ public class InventoryPropertyHandler implements PropertyHandler {
 	@Override
 	public boolean decorateDataObject(int entity, DataObject dataObject, boolean force) {
 		if (inventoryMapper.has(entity)) {
-			dataObject.set(MessageCodes.PROPERTY_INVENTORY, inventoryMapper.get(entity).items);
-			return true;
+			Inventory inventory = inventoryMapper.get(entity);
+			SyncHistory syncHistory = syncHistoryMapper.get(entity);
+
+			boolean inventoryChanged = false;
+			if (!force && syncHistory != null && syncHistory.syncedValues.containsKey(Properties.INVENTORY)) {
+				int lastSyncedItemsHashCode = (int) syncHistory.syncedValues.get(Properties.INVENTORY);
+				inventoryChanged = Arrays.hashCode(inventory.items) != lastSyncedItemsHashCode;
+			}
+			if (force || inventoryChanged) {
+				dataObject.set(MessageCodes.PROPERTY_INVENTORY, inventory.items);
+				if (syncHistory != null) {
+					syncHistory.syncedValues.put(Properties.INVENTORY, Arrays.hashCode(inventory.items));
+				}
+				return true;
+			}
 		}
 		return false;
 	}
