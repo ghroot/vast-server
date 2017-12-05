d61df5bce889255fca13f14489f1b8d9e1996daf
Marcus Lagerström <>
2017-12-05 20:45:20 +0100

    No need to keep sync history for inventory.

--- src/main/java/com/vast/property/InventoryPropertyHandler.java
+++ src/main/java/com/vast/property/InventoryPropertyHandler.java
@@ -5,13 +5,9 @@ import com.nhnent.haste.protocol.data.DataObject;
 import com.vast.MessageCodes;
 import com.vast.Properties;
 import com.vast.component.Inventory;
-import com.vast.component.SyncHistory;
-
-import java.util.Arrays;
 
 public class InventoryPropertyHandler implements PropertyHandler {
 	private ComponentMapper<Inventory> inventoryMapper;
-	private ComponentMapper<SyncHistory> syncHistoryMapper;
 
 	@Override
 	public int getProperty() {
@@ -21,21 +17,8 @@ public class InventoryPropertyHandler implements PropertyHandler {
 	@Override
 	public boolean decorateDataObject(int entity, DataObject dataObject, boolean force) {
 		if (inventoryMapper.has(entity)) {
-			Inventory inventory = inventoryMapper.get(entity);
-			SyncHistory syncHistory = syncHistoryMapper.get(entity);
-
-			boolean inventoryChanged = true;
-			if (!force && syncHistory != null && syncHistory.syncedValues.containsKey(Properties.INVENTORY)) {
-				int lastSyncedItemsHashCode = (int) syncHistory.syncedValues.get(Properties.INVENTORY);
-				inventoryChanged = Arrays.hashCode(inventory.items) != lastSyncedItemsHashCode;
-			}
-			if (force || inventoryChanged) {
-				dataObject.set(MessageCodes.PROPERTY_INVENTORY, inventory.items);
-				if (syncHistory != null) {
-					syncHistory.syncedValues.put(Properties.INVENTORY, Arrays.hashCode(inventory.items));
-				}
-				return true;
-			}
+			dataObject.set(MessageCodes.PROPERTY_INVENTORY, inventoryMapper.get(entity).items);
+			return true;
 		}
 		return false;
 	}
