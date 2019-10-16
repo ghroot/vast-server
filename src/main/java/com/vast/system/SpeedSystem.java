526bc0fdd1f83ec7ee73a36ab672cc505a32ef5e
Marcus Lagerström <>
2019-10-16 23:40:16 +0200

    Optimized SpeedSystem.

--- src/main/java/com/vast/system/SpeedSystem.java
+++ src/main/java/com/vast/system/SpeedSystem.java
@@ -4,29 +4,25 @@ import com.artemis.Aspect;
 import com.artemis.ComponentMapper;
 import com.artemis.systems.IteratingSystem;
 import com.vast.component.Inventory;
-import com.vast.component.Player;
 import com.vast.component.Speed;
 
 public class SpeedSystem extends IteratingSystem {
 	private ComponentMapper<Speed> speedMapper;
-	private ComponentMapper<Player> playerMapper;
 	private ComponentMapper<Inventory> inventoryMapper;
 
 	public SpeedSystem() {
-		super(Aspect.all(Speed.class));
+		super(Aspect.all(Speed.class, Inventory.class));
 	}
 
 	@Override
 	protected void process(int entity) {
 		Speed speed = speedMapper.get(entity);
+		Inventory inventory = inventoryMapper.get(entity);
 
-		if (playerMapper.has(entity) && inventoryMapper.has(entity)) {
-			Inventory inventory = inventoryMapper.get(entity);
-			if ((float) inventory.getNumberOfItems() / inventory.capacity >= 0.75f) {
-				speed.modifier = 0.85f;
-			} else {
-				speed.modifier = 1.0f;
-			}
+		if ((float) inventory.getNumberOfItems() / inventory.capacity >= 0.75f) {
+			speed.modifier = 0.85f;
+		} else {
+			speed.modifier = 1.0f;
 		}
 	}
 }
