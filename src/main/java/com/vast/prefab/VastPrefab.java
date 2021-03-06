d8f6be4040efbe8f87ad9e0904be7a4fbd182cc9
Marcus Lagerström <marcus.lagerstrom@gmail.com>
2021-03-06 16:02:27 +0100

    Make VastPrefab abstract.

--- src/main/java/com/vast/prefab/VastPrefab.java
+++ src/main/java/com/vast/prefab/VastPrefab.java
@@ -4,7 +4,7 @@ import com.artemis.World;
 import com.artemis.prefab.JsonValuePrefabReader;
 import com.artemis.prefab.Prefab;
 
-public class VastPrefab extends Prefab {
+public abstract class VastPrefab extends Prefab {
     public VastPrefab(World world) {
         super(world, new JsonValuePrefabReader());
     }
